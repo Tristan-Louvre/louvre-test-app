ca7489f4c305c37d1cfe40fdad287b8ae6fb2546
BVJ <byron@wigroup.co.za>
2017-05-24 17:47:08 +0200

    Finished routing section.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroService } from './hero.service';
-import {RouterModule} from "@angular/router";
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroService } from './hero.service';
+
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      },
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent,
-    DashboardComponent
+    HeroesComponent
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]

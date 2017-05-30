f8b482e839d2218907f15c92268f039b3558a401
Tristan <byron.victor.jones@gmail.com>
2017-05-30 23:54:52 +0200

    Fixed my broken code.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import {HttpModule} from "@angular/http";
+import {HttpModule} from '@angular/http';
 
 import {AppRoutingModule} from './app-routing.module';
 
